b738f55f4b18193add43a9d394745bde1ce32a37
nuck <teoxysoft@gmail.com>
2025-09-04 00:38:21 -0400

    Fix TreeSitter C# query to match actual grammar node types
    Replace generic/incorrect node types with C#-specific grammar nodes:
    - Remove non-existent function_declaration, function_definition, class_definition
    - Add proper C# nodes: class_declaration, method_declaration, constructor_declaration, property_declaration
    - Use correct field syntax with (identifier) captures for symbol names
    - Enable successful parsing of C# source files with TreeSitter
    
    This resolves "Query error at index 1: NodeType" and enables full TreeSitter functionality.
    The `dotnet run ls` command now works perfectly with rich symbol extraction.

--- Core/TreeSitterQueries.cs
+++ Core/TreeSitterQueries.cs
@@ -5,11 +5,9 @@ namespace Thaum.Core.Services;
 public static class TreeSitterQueries
 {
     public static readonly string UniversalQuery = @"
-(function_declaration name: (identifier) @function.name) @function.body
-(method_declaration name: (identifier) @method.name) @method.body
-(function_definition name: (identifier) @function.name) @function.body
 (class_declaration name: (identifier) @class.name) @class.body
-(class_definition name: (identifier) @class.name) @class.body
-(method_definition name: (property_identifier) @method.name) @method.body
+(method_declaration name: (identifier) @method.name) @method.body
+(constructor_declaration name: (identifier) @constructor.name) @constructor.body
+(property_declaration name: (identifier) @property.name) @property.body
 ";
 }
